a376ba7309e003626c48e8b88a049fbd85569d9e
Douglas Volcato <douglasvolcato@gmail.com>
2022-08-23 16:58:02 -0300

    Update

--- src/scripts/verifyAuth.js
+++ src/scripts/verifyAuth.js
@@ -2,7 +2,7 @@ import { Api } from "../api/api";
 const verifyAuth = ()=>{
     if(!Api.keyJwt){
         console.log('false');
-        return false;
+        return true;
     }else{
         return true;
     }
